b872a748c362ea215dc16d9923564e0d5cc28f3a
Darrion <darrionn33@gmail.com>
2023-12-09 23:06:22 +0530

    started remaking it in React.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-[100dvh] flex items-center justify-center">
+      Hello, I'm in the process of being remade in React, hehe.
+    </div>
+  );
+}
+
+export default App;
